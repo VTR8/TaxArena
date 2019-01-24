fac7fdb9abf39088fca830eb9373952619f876cc
Ajit bharti <a.bharti0310@gmail.com>
2019-01-25 01:26:43 +0530

    gallery added

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
+
+const appRoutes: Routes = [
+  { path:"taxArena",component: CarouselComponent},
+  { path:"gallery",component: GalleryComponent },
+  { path: '',redirectTo: '/taxArena',pathMatch: 'full'}
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    CarouselComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
